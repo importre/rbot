d1eec4b53324ecfa147bbb6c1ef5fb8bcc74f68a
Jaewe Heo <jaeweheo@gmail.com>
2015-06-10 13:32:43 +0900

    add dummy files

---
+++ src/main/java/io/github/importre/rbot/BuildConfig.java
@@ -0,0 +1,8 @@
+package io.github.importre.rbot;
+
+public class BuildConfig {
+    public static final String VERSION = "";
+    public static final String DB_NAME= "";
+    public static final String INSERT_SQL = "";
+    public static final String SELECT_SQL = "";
+}
